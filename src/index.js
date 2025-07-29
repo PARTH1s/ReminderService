d92af339318fe4750ed417eaff0755cd873910ba
Parth <parthsahani4786@gmail.com>
2025-07-29 23:21:06 +0530

    Initial Setup of Project

---
+++ src/index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+
+const bodyParser = require("body-parser");
+
+const { PORT } = require("./config/server-config");
+
+// const ApiRoutes = require("./routes/index");
+
+
+const SetUpandStartServer = async () => {
+    // create the express object
+    const app = express();
+
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({ extended: true }));
+
+    //   app.use("/api", ApiRoutes);
+
+    app.listen(PORT, async () => {
+        console.log(`Server Started at ${PORT}`);
+    });
+};
+
+SetUpandStartServer(); 
