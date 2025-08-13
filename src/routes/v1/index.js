f4b9ebbd30e387deee0504405fb55343233a50ca
Parth <parthsahani4786@gmail.com>
2025-08-13 23:53:20 +0530

    "Add cron job to send pending email notifications every 2 minutes with status updates"

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -1,9 +1,9 @@
 const express = require('express');
-
 const { TicketController } = require('../../controllers/index');
 
 const router = express.Router();
 
+// Route to create a new notification ticket
 router.post("/tickets", TicketController.create);
 
 module.exports = router;
